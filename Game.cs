a828a0374c86f4a6a0a2ec1242119e65dde5d1e2
Sam Clavette <samclavette@yahoo.com>
2020-07-16 14:25:21 -0500

    I added a DIsplayRules method to the Game class. I also fixed an issue with the ChooseHumanOrAI method.

--- Game.cs
+++ Game.cs
@@ -9,39 +9,29 @@ namespace RPSLS
 {
     class Game
     {
-        public Player player1;
-        public Player player2;
+        public Player playerOne;
+        public Player playerTwo;
 
         public Game(string player1, string player2)
         {
-            player1 = new Human();
+            playerOne = new Human();
         }
 
         public void RunGame()
         {
-            int userInput = ChooseHumanOrAI();
-            if (userInput == 1)
-            {
-                PlayAgainstHuman();
-            }
-            else if (userInput == 2)
-            {
-                PlayAgainstAI();
-            }
-            else if (userInput != 1 | userInput != 2)
-            {
-                ChooseHumanOrAI();
-            }
+            DisplayRules();
+            ChooseHumanOrAI();
         }
 
         public void DisplayRules()
         {
-
+            Console.WriteLine("You will be playing a variation of rock-paper-scissors with 2 (lizard,Spock) added gestures against an opponent. It will be a best of 3 series. Press any key to begin.");
+            Console.ReadLine();
         }
 
-        public string ChooseHumanOrAI()
+        public void ChooseHumanOrAI()
         {
-            Console.WriteLine("Please choose a number:/n1.) Single player/n2. Mulitplayer");
+            Console.WriteLine("Please choose a number:/n1.) Single player/n2. Multiplayer");
             string userInput = Console.ReadLine();
             if (userInput == "1")
             {
