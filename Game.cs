9f1110f983b052a8c576cc76119a5604e6e7b915
Sam Clavette <samclavette@yahoo.com>
2020-07-16 11:10:06 -0500

    I created a Game class and added member variables as well as 4 methods. I finished a method that takes user input to decide whether the opponent is a human or AI.

---
+++ Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        public string player1;
+        public string player2;
+
+        public Game(string player1, string player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public void RunGame()
+        {
+
+        }
+
+        public void ChooseHumanOrAI()
+        {
+            Console.WriteLine("Please choose a number:/n1.) Single player/n2. Mulitplayer");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void PlayAgainstHuman()
+        {
+
+        }
+
+        public void PlayAgainstAI()
+        {
+
+        }
+            
+    }
+}
